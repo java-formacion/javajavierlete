39f3f7fecc7b89a0cbd9c10c47e2d289358624bd
java <java@PORT-Z15.ipartekaula.com>
2018-01-09 13:46:44 +0100

    TODO update

--- EjemploPOO/src/com/ipartek/formacion/ejemplopoo/tipos/PuntoException.java
+++ EjemploPOO/src/com/ipartek/formacion/ejemplopoo/tipos/PuntoException.java
@@ -9,27 +9,27 @@ public class PuntoException extends RuntimeException {
 
 	public PuntoException() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public PuntoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public PuntoException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public PuntoException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public PuntoException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 }
