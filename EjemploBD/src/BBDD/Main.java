516e13c6f8e766c86c0f4e6c7fd0bbe256a7207a
java <java@PORT-Z13.ipartekaula.com>
2018-01-10 08:07:40 +0100

    hjjkkj

--- EjemploBD/src/BBDD/Main.java
+++ EjemploBD/src/BBDD/Main.java
@@ -6,43 +6,45 @@ import java.util.Iterator;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException{
 		
-		String url = "jdbc:sqlite:C:\\desarrolloRedes\\javajavierlete\\EjemploBD\\BBDD\\ex1.db";
+		String url = "jdbc:sqlite:BBDD\\ex1.db";
 		Connection con;
-		try {
+		
 			con = DriverManager.getConnection(url);
 			Statement st = con.createStatement();
 			String sql="SELECT * FROM tbl1";
 			ResultSet rs = st.executeQuery(sql);
 			
-			ResultSetMetaData rsmd = rs.getMetaData();
+			mostrarRegistros(rs);
+			
+			
+			
+			
+			}
+
+	private static void mostrarRegistros(ResultSet rs) throws SQLException {
+		ResultSetMetaData rsmd = rs.getMetaData();
+		
+		for (int i = 1; i <=rsmd.getColumnCount(); i++) {
+			
+			System.out.print(rsmd.getColumnName(i)+ "  ");
+		}
+		System.out.println();
+		while(rs.next()) {
 			
 			for (int i = 1; i <=rsmd.getColumnCount(); i++) {
 				
-				System.out.print(rsmd.getColumnName(i)+ "  ");
+				System.out.print(rs.getString(i)+ "  ");
 			}
 			System.out.println();
-			while(rs.next()) {
-				
-				for (int i = 1; i <=rsmd.getColumnCount(); i++) {
-					
-					System.out.print(rs.getString(i)+ "  ");
-				}
-				System.out.println();
-			}
-			
-			
-			
-			
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		
-		
-		
 	}
 
-}
+	}
+	
+	
+	
+	
+	
+	
