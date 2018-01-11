251c4206e47903e356402d753fcbdbf978f14093
java <java@PORT-Z6.ipartekaula.com>
2018-01-11 12:15:09 +0100

    Capa de acceso a datos inicial con acceso a usuario

---
+++ AccesoDatos/src/com/ipartek/formacion/ejemplocapas/accesodatos/DAOUsuarioArrayList.java
@@ -0,0 +1,80 @@
+package com.ipartek.formacion.ejemplocapas.accesodatos;
+
+import java.util.ArrayList;
+
+import com.ipartek.formacion.ejemplocapas.entidades.Usuario;
+
+public class DAOUsuarioArrayList implements DAOUsuario {
+
+	private ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+
+	@Override
+	public void alta(Usuario usuario) {
+
+		if (obtenerUsuarioPorEmail(usuario.getEmail()) != null) {
+			throw new AccesoDatosException("Ya existe un usuario con el email " + usuario.getEmail());
+		}
+		usuarios.add(usuario);
+
+	}
+
+	@Override
+	public void baja(Usuario usuario) {
+
+		usuarios.remove(usuario);
+
+	}
+
+	@Override
+	public void modificacion(Usuario usuario) {
+
+		Usuario u;
+
+		for (int i = 0; i < usuarios.size(); i++) {
+			u = usuarios.get(i);
+			if (u.getId() == usuario.getId()) {
+				u.setApellidos(usuario.getApellidos());
+				u.setDni(usuario.getDni());
+				u.setEmail(usuario.getEmail());
+				u.setNombre(usuario.getNombre());
+				u.setPassword(usuario.getPassword());
+			}
+		}
+
+	}
+
+	@Override
+	public Usuario[] obtenerUsuarios() {
+
+		return (Usuario[]) usuarios.toArray();
+
+	}
+
+	@Override
+	public Usuario obtenerUsuarioPorId(long id) {
+
+		for (Usuario u : usuarios) {
+			if (id == u.getId()) {
+				return u;
+			}
+		}
+		return null;
+
+	}
+
+	@Override
+	public Usuario obtenerUsuarioPorEmail(String email) {
+
+		if (email == null) {
+			return null;
+		}
+		for (Usuario u : usuarios) {
+			if (u.getEmail() != null && u.getEmail().equals(email)) {
+				return u;
+			}
+		}
+		return null;
+
+	}
+
+}
