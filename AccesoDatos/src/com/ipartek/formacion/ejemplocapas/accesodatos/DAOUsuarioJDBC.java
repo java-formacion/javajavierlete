8e584106d5c30a8371094c1227020cf8206af3e9
javierlete <jglete@gmail.com>
2018-01-15 11:13:37 +0100

    Resto de operaciones de JDBC y pruebas del mismo

--- AccesoDatos/src/com/ipartek/formacion/ejemplocapas/accesodatos/DAOUsuarioJDBC.java
+++ AccesoDatos/src/com/ipartek/formacion/ejemplocapas/accesodatos/DAOUsuarioJDBC.java
@@ -11,8 +11,22 @@ public class DAOUsuarioJDBC implements DAOUsuario {
 			"INSERT INTO usuarios " +
 			"(dni, email, password, nombre, apellidos)" +
 			"VALUES (?, ?, ?, ?, ?)";
+	private static final String SQL_UPDATE =
+			"UPDATE usuarios SET "+
+			"dni=?, email=?, password=?, nombre=?, apellidos=? "+
+			"WHERE id=?";
+	private static final String SQL_DELETE =
+			"DELETE FROM usuarios WHERE id=?";
+	
 	private static final String SQL_SELECT = 
 			"SELECT id, dni, email, password, nombre, apellidos FROM usuarios ";
+	private static final String SQL_SELECT_ID = 
+			"SELECT id, dni, email, password, nombre, apellidos "+
+			"FROM usuarios WHERE id=?";
+	private static final String SQL_SELECT_EMAIL = 
+			"SELECT id, dni, email, password, nombre, apellidos "+
+			"FROM usuarios WHERE email=?";
+
 	private final String url;
 	
 	public DAOUsuarioJDBC(String url) {
@@ -57,14 +71,71 @@ public class DAOUsuarioJDBC implements DAOUsuario {
 
 	@Override
 	public void baja(Usuario usuario) {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement ps = null;
 		
+		try {
+			con = DriverManager.getConnection(url);
+			
+			ps = con.prepareStatement(SQL_DELETE);
+			
+			ps.setLong(1, usuario.getId());
+			
+			int num = ps.executeUpdate();
+			
+			if(num != 1)
+				throw new AccesoDatosException(
+						"El borrado ha devuelto un resultado diferente de 1");
+		} catch (SQLException e) {
+			throw new AccesoDatosException(
+					"Error al acceder a la base de datos", e);
+		} finally {
+			try {
+				if(ps != null)
+					ps.close();
+				if(con != null)
+					con.close();
+			} catch (SQLException e) {
+				throw new AccesoDatosException("Ha habido un error al cerrar", e);
+			}
+		}
 	}
 
 	@Override
 	public void modificacion(Usuario usuario) {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement ps = null;
 		
+		try {
+			con = DriverManager.getConnection(url);
+			
+			ps = con.prepareStatement(SQL_UPDATE);
+			
+			ps.setString(1, usuario.getDni());
+			ps.setString(2, usuario.getEmail());
+			ps.setString(3, usuario.getPassword());
+			ps.setString(4, usuario.getNombre());
+			ps.setString(5, usuario.getApellidos());
+			ps.setLong(6, usuario.getId());
+			
+			int num = ps.executeUpdate();
+			
+			if(num != 1)
+				throw new AccesoDatosException(
+						"La actualización ha devuelto un resultado diferente de 1");
+		} catch (SQLException e) {
+			throw new AccesoDatosException(
+					"Error al acceder a la base de datos", e);
+		} finally {
+			try {
+				if(ps != null)
+					ps.close();
+				if(con != null)
+					con.close();
+			} catch (SQLException e) {
+				throw new AccesoDatosException("Ha habido un error al cerrar", e);
+			}
+		}
 	}
 
 	@Override
@@ -86,7 +157,7 @@ public class DAOUsuarioJDBC implements DAOUsuario {
 			
 			while(rs.next()) {
 				u = new Usuario(
-						rs.getInt("id"),
+						rs.getLong("id"),
 						rs.getString("dni"),
 						rs.getString("email"),
 						rs.getString("password"),
@@ -116,14 +187,96 @@ public class DAOUsuarioJDBC implements DAOUsuario {
 
 	@Override
 	public Usuario obtenerUsuarioPorId(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			con = DriverManager.getConnection(url);
+			
+			ps = con.prepareStatement(SQL_SELECT_ID);
+			
+			ps.setLong(1, id);
+			
+			rs = ps.executeQuery();
+			
+			Usuario u;
+			
+			if(rs.next()) {
+				u = new Usuario(
+						rs.getLong("id"),
+						rs.getString("dni"),
+						rs.getString("email"),
+						rs.getString("password"),
+						rs.getString("nombre"),
+						rs.getString("apellidos"));
+				
+				return u;
+			} 
+			
+			return null;
+		} catch (SQLException e) {
+			throw new AccesoDatosException(
+					"Error al acceder a la base de datos", e);
+		} finally {
+			try {
+				if(rs != null)
+					rs.close();
+				if(ps != null)
+					ps.close();
+				if(con != null)
+					con.close();
+			} catch (SQLException e) {
+				throw new AccesoDatosException("Ha habido un error al cerrar", e);
+			}
+		}
 	}
 
 	@Override
 	public Usuario obtenerUsuarioPorEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			con = DriverManager.getConnection(url);
+			
+			ps = con.prepareStatement(SQL_SELECT_EMAIL);
+			
+			ps.setString(1, email);
+			
+			rs = ps.executeQuery();
+			
+			Usuario u;
+			
+			if(rs.next()) {
+				u = new Usuario(
+						rs.getLong("id"),
+						rs.getString("dni"),
+						rs.getString("email"),
+						rs.getString("password"),
+						rs.getString("nombre"),
+						rs.getString("apellidos"));
+				
+				return u;
+			} 
+			
+			return null;
+		} catch (SQLException e) {
+			throw new AccesoDatosException(
+					"Error al acceder a la base de datos", e);
+		} finally {
+			try {
+				if(rs != null)
+					rs.close();
+				if(ps != null)
+					ps.close();
+				if(con != null)
+					con.close();
+			} catch (SQLException e) {
+				throw new AccesoDatosException("Ha habido un error al cerrar", e);
+			}
+		}
 	}
 
 }
--- PresentacionConsola/src/com/ipartek/formacion/ejemplocapas/presentacionconsola/Main.java
+++ PresentacionConsola/src/com/ipartek/formacion/ejemplocapas/presentacionconsola/Main.java
@@ -13,6 +13,61 @@ import com.ipartek.formacion.ejemplocapas.entidades.Usuario;
 public class Main {
 	
 	public static void main(String[] args) {
+		DAOUsuario daoUsuario = //new DAOUsuarioArrayList(); 
+				new DAOUsuarioJDBC(
+				"jdbc:sqlite:..\\AccesoDatos\\bdd\\ejemplocapas.s3db");
+		
+		UsuariosComponente uc = new UsuariosComponente(daoUsuario);
+		
+		Usuario usuario1 = new Usuario(
+				1, 
+				"12345678Z", 
+				"javierlete@miemail.com", 
+				"contraseña", 
+				"Javier", 
+				"Lete");
+		
+		Usuario usuario2 = new Usuario(
+				2, 
+				"12345678Z", 
+				"pepeperez@miemail.com", 
+				"contraseña", 
+				"Pepe", 
+				"Pérez");
+		
+		daoUsuario.alta(usuario1);
+		
+		daoUsuario.alta(usuario2);
+
+		uc.mostrarUsuarios();
+		
+		Usuario usuario = daoUsuario.obtenerUsuarioPorEmail("pepeperez@miemail.com");
+		
+		System.out.println("El usuario cuyo email es nuevo... es ");
+		
+		uc.mostrarUsuario(usuario);
+		
+		usuario.setNombre("Nuevo");
+		usuario.setApellidos("Nuevez");
+		
+		daoUsuario.modificacion(usuario);
+		
+		uc.mostrarUsuarios();
+		
+		usuario = daoUsuario.obtenerUsuarioPorId(usuario.getId());
+		
+		System.out.println("Usuario por ID");
+		
+		uc.mostrarUsuario(usuario);
+		
+		daoUsuario.baja(usuario);
+		
+		daoUsuario.baja(daoUsuario.obtenerUsuarioPorEmail("javierlete@miemail.com"));
+		
+		uc.mostrarUsuarios();
+	}
+	
+	public static void mainPruebaUsuarioJDBC(String[] args) {
 		DAOUsuario daoUsuario = new DAOUsuarioJDBC(
 				"jdbc:sqlite:..\\AccesoDatos\\bdd\\ejemplocapas.s3db");
 		
