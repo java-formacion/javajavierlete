fe8a09116dd6e9e2f5f806c42b1484c3609282b6
javierlete <jglete@gmail.com>
2018-01-15 09:07:50 +0100

    Nuevo acceso a datos para Producto

--- AccesoDatos/src/com/ipartek/formacion/ejemplocapas/accesodatos/DAOUsuarioArrayList.java
+++ AccesoDatos/src/com/ipartek/formacion/ejemplocapas/accesodatos/DAOUsuarioArrayList.java
@@ -24,19 +24,17 @@ public class DAOUsuarioArrayList implements DAOUsuario {
 
 	@Override
 	public void modificacion(Usuario usuario) {
-		Usuario u;
-		
-		for(int i = 0; i < usuarios.size(); i++) {
-			u = usuarios.get(i);
+		Usuario u = obtenerUsuarioPorId(usuario.getId());
 			
-			if(u.getId() == usuario.getId()) {
-				u.setApellidos(usuario.getApellidos());
-				u.setDni(usuario.getDni());
-				u.setEmail(usuario.getEmail());
-				u.setNombre(usuario.getNombre());
-				u.setPassword(usuario.getPassword());
-			}
-		}		
+		if(u != null) {
+			u.setApellidos(usuario.getApellidos());
+			u.setDni(usuario.getDni());
+			u.setEmail(usuario.getEmail());
+			u.setNombre(usuario.getNombre());
+			u.setPassword(usuario.getPassword());
+		} else {
+			throw new AccesoDatosException("No se ha encontrado el usuario a modificar");
+		}
 	}
 
 	@Override
--- PresentacionConsola/src/com/ipartek/formacion/ejemplocapas/presentacionconsola/Main.java
+++ PresentacionConsola/src/com/ipartek/formacion/ejemplocapas/presentacionconsola/Main.java
@@ -1,12 +1,52 @@
 package com.ipartek.formacion.ejemplocapas.presentacionconsola;
 
+import java.math.BigDecimal;
+
+import com.ipartek.formacion.ejemplocapas.accesodatos.DAOProducto;
+import com.ipartek.formacion.ejemplocapas.accesodatos.DAOProductoArrayList;
 import com.ipartek.formacion.ejemplocapas.accesodatos.DAOUsuario;
 import com.ipartek.formacion.ejemplocapas.accesodatos.DAOUsuarioArrayList;
+import com.ipartek.formacion.ejemplocapas.entidades.Producto;
 import com.ipartek.formacion.ejemplocapas.entidades.Usuario;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		DAOProducto daoProducto = new DAOProductoArrayList();
+		
+		ProductosComponente pc = new ProductosComponente(daoProducto);
+		
+		for(int i = 1; i <= 3; i++)
+			daoProducto.alta(new Producto(
+				i,
+				"Producto" + i,
+				"Descripción" + i,
+				new BigDecimal("" + i + i + "." + i + i)
+				)
+			);
+		
+		pc.mostrarProductos();
+		
+		Producto producto2 = daoProducto.obtenerProductoPorId(1);
+		
+		pc.mostrarProducto(producto2);
+				
+		producto2.setNombre("MODIFICADO");
+		producto2.setPrecio(new BigDecimal("50.50"));
+		
+		daoProducto.modificacion(producto2);
+		
+		pc.mostrarProductos();
+		
+		Producto producto1 = new Producto(
+				2, "Producto2", "Descripción2", new BigDecimal("22.22")
+				);
+
+		daoProducto.baja(producto1);
+		
+		pc.mostrarProductos();
+	}
+	public static void mainUsuario(String[] args) {
 		DAOUsuario daoUsuario = new DAOUsuarioArrayList();
 		
 		UsuariosComponente uc = new UsuariosComponente(daoUsuario);
