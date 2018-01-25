a0eed4301ecf1ee3c1502f8cee996bd2c86a2037
java <java@PORT-Z8.ipartekaula.com>
2018-01-25 08:21:44 +0100

    EjemploMvc

---
+++ EjemploMVC/src/com/ipartek/formacion/ejemplomvc/controladores/IndexServlet.java
@@ -0,0 +1,34 @@
+package com.ipartek.formacion.ejemplomvc.controladores;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class IndexServlet extends HttpServlet {
+	private static final String LOGIN_JSP = "WEB-INF/jsps/login.jsp";
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.getWriter().println(request.getServletPath());
+		RequestDispatcher loginDispatcher = request.getRequestDispatcher(LOGIN_JSP);
+		
+		switch(request.getServletPath()) {
+		case "/":
+			loginDispatcher.forward(request, response);
+			break;
+		default:
+			response.getWriter().println(request.getServletPath());
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
