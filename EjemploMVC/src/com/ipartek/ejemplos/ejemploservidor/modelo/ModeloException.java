581ed2280ff5a749f8bd51741ec2c95125c292fa
java <java@PORT-Z15.ipartekaula.com>
2018-01-30 13:55:25 +0100

    aaa

--- EjemploMVC/src/com/ipartek/ejemplos/ejemploservidor/modelo/ModeloException.java
+++ EjemploMVC/src/com/ipartek/ejemplos/ejemploservidor/modelo/ModeloException.java
@@ -5,15 +5,17 @@ public class ModeloException extends RuntimeException {
 	private static final long serialVersionUID = -409171339864714188L;
 
 	public ModeloException() {
-		
 	}
 
 	public ModeloException(String message) {
 		super(message);
+		
 	}
 
 	public ModeloException(Throwable cause) {
-		super(cause);	}
+		super(cause);
+		
+	}
 
 	public ModeloException(String message, Throwable cause) {
 		super(message, cause);
@@ -21,7 +23,6 @@ public class ModeloException extends RuntimeException {
 
 	public ModeloException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		
 	}
 
 }
